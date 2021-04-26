793e2e16d137124d1d9ecec2ca1f27b6218b6d51
maruo0501 <sagiri.maruo@gmail.com>
2021-04-26 22:57:37 +0900

    create nuxt-app

---
+++ components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/fun">Fun</NuxtLink>
+      <!-- <NuxtLink to="/users/list">User List</NuxtLink> -->
+      <NuxtLink :to="{name: 'users-list'}">Users</NuxtLink>
+    </nav>
+  </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>About Page</h1>
+    <!-- <nuxt-link to="/">Home Page</nuxt-link> -->
+  </main>
+</template>
