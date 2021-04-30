e8dd0732d5c41a52b44379fd9600adb7051b78ac
maruo0501 <sagiri.maruo@gmail.com>
2021-04-30 11:20:08 +0900

    Add 検索機能追加

--- store/todos.js
+++ store/todos.js
@@ -1,17 +1,15 @@
 export const state = () => ({
-  // todos: [
-  //   {content: "テスト", created: '2020-04-30 17:00', state: "作業前"},
-  //   {content: "コーディング", created: '2020-04-30 16:00', state: "作業中"},
-  //   {content: "環境構築", created: '2020-04-30 15:30', state: "完了"}
-  // ]
   todos: []
 })
+export const getters = {
+  getTodo: (state) => {
+    return state.todos
+  }
+}
 export const mutations = {
   add(state, text) {
     state.todos.push({
       text: text,
-      done: false,
-      id: state.todos.length + 1,
     })
   },
   remove(state, { todo }) {
